c180001d20c62f8de5e906fc79823bff5a91b5c1
Alex Philayvanh <aphilay@gmail.com>
2019-11-01 23:28:48 -0700

    initial project upload' git push

---
+++ src/screens/AccountScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const AccountScreen = props => {
+  <Text style={{ fontSize: 48 }}>AccountScreen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default AccountScreen;
