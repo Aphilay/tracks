5688024359e9c9af275e96434628b3d7c60d322a
Alex Philayvanh <aphilay@gmail.com>
2020-02-09 19:39:12 -0800

    It's been a while, but finally wired up the six different screens using stak/bottom tab navigator--welcome back

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 
-const AccountScreen = props => {
-  <Text style={{ fontSize: 48 }}>AccountScreen</Text>;
+const AccountScreen = () => {
+  return (
+    <>
+      <Text style={{ fontSize: 48 }}>AccountScreen</Text>
+    </>
+  );
 };
 
 const styles = StyleSheet.create({});
